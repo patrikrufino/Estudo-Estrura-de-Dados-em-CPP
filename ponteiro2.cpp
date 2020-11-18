5e23493b3f53f690148dfb03682661d72826ed37
Patrik Rufino <58515198+patrikrufino@users.noreply.github.com>
2020-11-18 07:12:39 -0300

    Add files via upload

---
+++ ponteiro2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int valor1;
+    int *pValor1;
+    char valor2;
+    char *pValor2;
+
+    pValor1= &valor1;
+    pValor2= &valor2;
+
+    printf("Endereco de pValor1 = %d\n", pValor1);
+    pValor1++;
+    printf("Endereco novo de pValor1 = %d\n", pValor1);
+
+    printf("Endereco de pValor2 = %d\n", pValor2);
+    pValor2++;
+    printf("Endereco novo de pValor2 = %d\n", pValor2);
+    //return 0;
+};
